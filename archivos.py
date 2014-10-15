3462897461757ca55b32e11e40d7f6b1d3ab3deb
ks7000 <jimmyolano@hotmail.com>
2014-10-15 15:50:44 -0430

    Si el archivo no existe LO CREA con el comando 'touch'

--- archivos.py
+++ archivos.py
@@ -1,9 +1,11 @@
 # -*- encoding=utf-8 -*-
 import os
+import sys
 def lee(*args):
     for i in args:
         if isinstance(i,str):
-            if os.access(i, os.R_OK): 
+            #if os.access(i, os.R_OK): 
+            if existe_archivo(i):
                 arch = raw_input("Introduzca el nombre del archivo a copiar:");
                 if arch<> "":
                     copiar = False                    
@@ -16,7 +18,8 @@ def lee(*args):
                     if copiar:                    
                         f = open(i)
                         g = open(arch, "w")
-                        g.write("Este archivo fue copiado de " , os.path.
+                        g.write("Este archivo fue copiado de ")
+                        g.write(os.path.realpath(i))
                         for linea in f:
                             print linea
                             g.write(linea)
@@ -25,5 +28,17 @@ def lee(*args):
             else:
                 print "El archivo no existe."
 
+def existe_archivo(archivo):
+    if os.path.exists(archivo):
+        return True
+    else:
+        resp = raw_input("El archivo NO existe, ¿Desea crearlo? [s/n]");
+        if resp == "s" or resp == "S":
+            if sys.platform == "linux2":
+                os.system('touch ' + archivo )
+            return True
+        else:
+            return False
+        
 archivo = raw_input("Introduzca el(los) nombre(s) de el(os) archivo(s) original(es):");
 lee(archivo)
