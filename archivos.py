1952bd9d2e782125ab2f8210ba86db17b00fcf40
ks7000 <jimmyolano@hotmail.com>
2014-10-15 15:19:23 -0430

    Pregunta el nombre del archivo a mostrar y lo copia en otro archivo

--- archivos.py
+++ archivos.py
@@ -1,9 +1,29 @@
+# -*- encoding=utf-8 -*-
+import os
 def lee(*args):
     for i in args:
         if isinstance(i,str):
-            f = open(i)
-            for linea in f:
-                print linea
-            f.close()
+            if os.access(i, os.R_OK): 
+                arch = raw_input("Introduzca el nombre del archivo a copiar:");
+                if arch<> "":
+                    copiar = False                    
+                    if os.path.exists(arch):
+                        resp = raw_input("El archivo existe, ¿Desea sobreescribirlo? [s/n]");
+                        if resp == "s" or resp == "S":
+                            copiar = True
+                    else:
+                        copiar = True
+                    if copiar:                    
+                        f = open(i)
+                        g = open(arch, "w")
+                        g.write("Este archivo fue copiado de " , os.path.
+                        for linea in f:
+                            print linea
+                            g.write(linea)
+                        f.close()
+                        g.close()
+            else:
+                print "El archivo no existe."
 
-lee("archivo.txt")
+archivo = raw_input("Introduzca el(los) nombre(s) de el(os) archivo(s) original(es):");
+lee(archivo)
