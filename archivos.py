f90f9f869e7e01a6f833953f852baf6015e2855f
ks7000 <jimmyolano@hotmail.com>
2014-10-17 15:19:06 -0430

    Le agrego 'click.pocoo.org' a 'archivos.py'

--- archivos.py
+++ archivos.py
@@ -1,10 +1,14 @@
 # -*- encoding=utf-8 -*-
+import click
 import os
 import sys
 import glob
+#import pdb
+#pdb.set_trace()
 def lee(*args):
     for i in args:
-        if isinstance(i,str):
+        #print "dentro de funcion lee()", type(i)
+        if isinstance(i,unicode) or isinstance(i,str):
             #if os.access(i, os.R_OK): 
             if existe_archivo(i):
                 arch = raw_input("Introduzca el nombre del archivo a copiar:");
@@ -55,11 +59,22 @@ def lee_archivo_varias_lineas(archivo):
 #        cad = i
 #        if string.find(cad,"*.txt"):
 #            print cad
-        
-            
-if sys.platform == "linux2":
-    os.system('clear')
-#lista_txt()
-print glob.glob('*.txt')
-archivo = raw_input("Introduzca el(los) nombre(s) de el(os) archivo(s) original(es):");
-lee(archivo)
+
+@click.command()
+@click.option('--archivo_original', default='original.txt', prompt='Introduzca el nombre del archivo ORIGINAL a copiar', help='Permite indicar el nombre del archivo original.')
+def principal(archivo_original):
+    if sys.platform == "linux2":
+        os.system('clear')
+        #lista_txt()
+        print glob.glob('*.txt')
+        #archivo = raw_input("Introduzca el(los) nombre(s) de el(os) archivo(s) original(es):");
+        #print archivo_original
+        #print type(archivo_original)
+        #copia=""
+        #print type(copia)
+        #copia=archivo_original
+        #lee(copia)
+        lee(archivo_original)
+
+if __name__ == '__main__':
+    principal()
